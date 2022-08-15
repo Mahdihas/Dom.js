25498148f188411005add70f58e0ccdedcec2ea8
Mahdihas <mahdihasan038@gmail.com>
2022-08-15 10:09:43 +0600

    dddd

--- buble.js/buble.js
+++ buble.js/buble.js
@@ -9,7 +9,7 @@
 //     console.log('section-containeer')
 // })
 
-///  
+// /  
 // const items = document.getElementsByClassName('item');
 // console.log(items);
 // for(const item of items){
@@ -18,7 +18,7 @@
 
 // })    
 // }
-//for display node// 
+// for display node// 
 // const items = document.getElementsByClassName('item');
 // console.log(items);
 // for(const item of items){
@@ -38,16 +38,107 @@
 // }
 
 // === event deligate ===  ////
-// const listContainer =document.getElementById('list-container').addEventListener('click' ,function(event){
-//     // console.log(event.target)
-//     event.target.parentNode.removeChild(event.target);
-// })
+const listContainer =document.getElementById('list-container').addEventListener('click' ,function(event){
+    // console.log(event.target)
+    event.target.parentNode.removeChild(event.target);
+})
 
 
-// document.getElementById('add-item').addEventListener('click', function(){
-//     const listContainer = document.getElementById('list-container');
-//     const li = document.createElement('li');
-//     li.classList.add('item');
-//     li.innerText = 'brand new item added';
-//     listContainer.appendChild(li);
-// })
+document.getElementById('add-item').addEventListener('click', function(){
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.innerText = 'brand new item added';
+    listContainer.appendChild(li);
+})
+
+
+
+
+document.getElementById('item1').addEventListener('click', function(){
+    document.getElementById('item1').style.display = 'none';
+    
+})
+document.getElementById('item2').addEventListener('click', function(){
+    document.getElementById('item2').style.display = 'none';
+    
+})
+document.getElementById('item3').addEventListener('click', function(){
+    document.getElementById('item3').style.display = 'none';
+    
+})
+document.getElementById('item4').addEventListener('click', function(){
+    document.getElementById('item4').style.display = 'none';
+    
+})
+document.getElementById('item5').addEventListener('click', function(){
+    document.getElementById('item5').style.display = 'none';
+    
+})
+document.getElementById('item6').addEventListener('click', function(){
+    document.getElementById('item6').style.display = 'none';
+    
+})
+
+document.getElementById('add-item1').addEventListener('click', function(){
+    document.createElement
+    document.getElementById('item1').style.display = 'block'
+    document.getElementById('item2').style.display = 'block'
+    document.getElementById('item3').style.display = 'block'
+    document.getElementById('item4').style.display = 'block'
+    document.getElementById('item5').style.display = 'block'
+    document.getElementById('item6').style.display = 'block'
+    
+})
+
+//  document.getElementById('addColor1').addEventListener('click', function(){
+//     document.getElementById('section').style.backgroundColor = 'red';
+//     document.getElementById('addColor1').style.backgroundColor = 'red';
+//     document.getElementById('addColor1').style.color = 'white';
+//     document.getElementById('addColor1').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+
+//  })
+// function Color (){
+//     document.getElementById('section').style.backgroundColor = "pink";
+//     document.getElementById('addColor2').style.backgroundColor = 'pink';
+//     document.getElementById('addColor2').style.color = 'white';
+//     document.getElementById('addColor2').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+
+// }
+
+// document.getElementById('addColor3').addEventListener('click', function(){
+//     document.getElementById('section').style.backgroundColor = 'blue';
+//     document.getElementById('addColor3').style.backgroundColor = 'blue';
+//     document.getElementById('addColor3').style.color = 'white';
+//     document.getElementById('addColor3').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+//  })
+//  function Color4 (){
+//     document.getElementById('section').style.backgroundColor = "orange";
+//     document.getElementById('addColor4').style.backgroundColor = 'orange';
+//     document.getElementById('addColor4').style.color = 'white';
+//     document.getElementById('addColor4').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+// }
+// document.getElementById('addColor5').addEventListener('click', function(){
+//     document.getElementById('section').style.backgroundColor = 'tomato';
+//     document.getElementById('addColor5').style.backgroundColor = 'tomato';
+//     document.getElementById('addColor5').style.color = 'white';
+//     document.getElementById('addColor5').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+//  })
+//  function Color6 (){
+//     document.getElementById('section').style.backgroundColor = "maroon";
+//     document.getElementById('addColor6').style.backgroundColor = 'maroon';
+//     document.getElementById('addColor6').style.color = 'white';
+//     document.getElementById('addColor6').style.border = '1px solid white';
+//     document.getElementById('body').style.backgroundColor = 'black';
+
+// }
